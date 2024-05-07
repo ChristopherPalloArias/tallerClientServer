36c3b7d4bb5eeb5c8277ae373fedd193b72703a4
ChristopherPalloArias <cipallo@uce.edu.ec>
2024-05-07 16:14:23 -0500

    Add f

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader'],
       }
     ],
   },

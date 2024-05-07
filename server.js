8b74f190dd6bfb2172093ee96b6ff118c908e42c
ChristopherPalloArias <cipallo@uce.edu.ec>
2024-05-07 14:59:24 -0500

    Fix WebSocket

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const mysql = require('mysql');
 const cors = require('cors');
 
@@ -6,30 +8,41 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const db = mysql.createConnection({
-    host: "mysql-christopherobin.alwaysdata.net",
-    user: "358042_admin",
-    password: "YqUZn6T6AxLYc5k",
-    database: "christopherobin_practiceclientserver"
-})
+    host: 'mysql-christopherobin.alwaysdata.net',
+    user: '358042_admin',
+    password: 'YqUZn6T6AxLYc5k',
+    database: 'christopherobin_practiceclientserver'
+});
 
-app.post('/login', (req, res) =>{
-    const sql = "SELECT * FROM usuarios WHERE username = ? AND password =?";
+io.on('connection', socket => {
+    console.log('User connected: ' + socket.id);
 
-    db.query(sql, [req.body.email, req.body.password], (err, data) =>{
-        if(err) return res.json("Error");
-        if(data.length > 0){ 
-           return res.json("Login Successfully")
+    socket.on('disconnect', () => {
+        console.log('User disconnected: ' + socket.id);
+    });
+});
+
+app.post('/login', (req, res) => {
+    const sql = 'SELECT * FROM usuarios WHERE username = ? AND password = ?';
+
+    db.query(sql, [req.body.email, req.body.password], (err, data) => {
+        if (err) return res.json('Error');
+        if (data.length > 0) {
+            return res.json('Login Successfully');
         } else {
-            return res.json("Login Failed")
+            return res.json('Login Failed');
         }
-    })
-})
+    });
+});
 
 app.get('/', (req, res) => {
     res.send('Server is running.....');
 });
 
-app.listen(8081, () =>{
-    console.log("Listening..")
-})
+server.listen(8081, () => {
+    console.log('Listening..');
+});
--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from './Login';
 import HomePage from './HomePage'; // Importa el componente HomePage
+import io from 'socket.io-client';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io('http://tu-url-en-digitalocean');
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
 
   function handleLoginSuccess() {
     setIsLoggedIn(true);
@@ -11,7 +22,7 @@ function App() {
 
   return (
     <div>
-      {isLoggedIn ? <HomePage /> : <Login onLoginSuccess={handleLoginSuccess} />}
+      {isLoggedIn ? <HomePage socket={socket} /> : <Login onLoginSuccess={handleLoginSuccess} />}
     </div>
   );
 }
